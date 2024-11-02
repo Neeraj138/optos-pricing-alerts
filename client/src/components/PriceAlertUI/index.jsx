bf9e68c799a419d497a728717c13d02ae7caf746
Neeraj138 <neeraj.r@greyorange.com>
2024-11-03 02:18:07 +0530

    add env var

--- client/src/components/PriceAlertUI/index.jsx
+++ client/src/components/PriceAlertUI/index.jsx
@@ -85,7 +85,7 @@ const PriceAlertUI = () => {
 
   const handleSetAlert = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/alerts", {
+      const response = await fetch(`${import.meta.env.VITE_OPTOS_ALERTS_SERVER_URL}/api/alerts`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
